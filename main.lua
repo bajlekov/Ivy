d65351c98cff75bd66e231ad6fdef1998b8505c1
Unknown <g.bajlekov@gmail.com>
2019-02-27 12:01:41 +0100

    Rename application to Ivy

--- main.lua
+++ main.lua
@@ -1,12 +1,12 @@
 --[[
   Copyright (C) 2011-2018 G. Bajlekov
 
-    ImageFloat is free software: you can redistribute it and/or modify
+    Ivy is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    ImageFloat is distributed in the hope that it will be useful,
+    Ivy is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
@@ -15,7 +15,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
 
-print([[    ImageFloat 3
+print([[    Ivy
     Copyright (C) 2018  Galin Bajlekov
 
     This program is free software: you can redistribute it and/or modify
@@ -50,7 +50,7 @@ end
 
 
 assert(love.window.setMode(1280, 720, {resizable = true, vsync = true, minwidth = 1280, minheight = 720, msaa = 4} ))
-love.window.setTitle("ImageFloat 3: Initializing...")
+love.window.setTitle("Ivy: Initializing...")
 love.window.setIcon(love.image.newImageData("res/icon.png"))
 
 love.window.maximize()
@@ -160,7 +160,7 @@ function love.filedropped(file)
 
 	originalImage = require("io."..settings.imageLoader).read(file):toDevice(true)
 
-	love.window.setTitle("ImageFloat 3: "..( type(file) == "string" and file or file:getFilename() ))
+	love.window.setTitle("Ivy: "..( type(file) == "string" and file or file:getFilename() ))
 	exifData = require("io.exif").read(file)
 
 	local fileName = type(file) == "string" and file or file:getFilename() or "-"
--- setup.lua
+++ setup.lua
@@ -1,12 +1,12 @@
 --[[
   Copyright (C) 2011-2018 G. Bajlekov
 
-    ImageFloat is free software: you can redistribute it and/or modify
+    Ivy is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    ImageFloat is distributed in the hope that it will be useful,
+    Ivy is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
@@ -62,7 +62,7 @@ end
 
 require "tools.math"
 
-love.filesystem.setIdentity("ImageFloat3")
+love.filesystem.setIdentity("Ivy")
 
 -- expand debug library
 require "tools.debug"
