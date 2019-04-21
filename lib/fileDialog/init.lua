cc3197aba585456fb77c80f30626265bbb312fdb
Galin Bajlekov <g.bajlekov@gmail.com>
2019-04-21 22:12:50 +0200

    Provide file dialogs based on tiny file dialogs

---
+++ lib/fileDialog/init.lua
@@ -0,0 +1,90 @@
+--[[
+  Copyright (C) 2011-2019 G. Bajlekov
+
+    Ivy is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Ivy is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]
+
+local ffi = require "ffi"
+
+local fileDialog = {}
+
+local tfd = ffi.load("lib/fileDialog/Windows/tfd.dll")
+
+ffi.cdef[[
+	char* tinyfd_openFileDialog(
+		char const * const aTitle , /* NULL or "" */
+		char const * const aDefaultPathAndFile , /* NULL or "" */
+		int aNumOfFilterPatterns , /* 0 */
+		char ** aFilterPatterns , /* NULL | {"*.jpg","*.png"} */
+		char const * const aSingleFilterDescription , /* NULL | "image files" */
+		int aAllowMultipleSelects ) ; /* 0 or 1 */
+			/* in case of multiple files, the separator is | */
+			/* returns NULL on cancel */
+
+	char const * tinyfd_saveFileDialog(
+		char const * const aTitle , /* NULL or "" */
+		char const * const aDefaultPathAndFile , /* NULL or "" */
+		int aNumOfFilterPatterns , /* 0 */
+		char ** aFilterPatterns , /* NULL | {"*.jpg","*.png"} */
+		char const * const aSingleFilterDescription ) ; /* NULL | "text files" */
+			/* returns NULL on cancel */
+]]
+
+function fileDialog.fileOpen(title, path, filter, description)
+	if type(filter)=="string" then filter = {filter} end
+
+	local filterList
+	if type(filter)=="table" then
+		filterList = ffi.new("char*[?]", #filter)
+
+		for k, v in ipairs(filter) do
+			filter[k] = ffi.new("char[?]", #v, v) -- anchor filters in original array
+			filterList[k-1] = filter[k]
+		end
+	end
+
+	return ffi.string(tfd.tinyfd_openFileDialog(
+		title,
+		path,
+		type(filter)=="table" and #filter or 0,
+		filterList,
+		description,
+		0))
+end
+
+function fileDialog.fileSave(title, path, filter, description)
+	if type(filter)=="string" then filter = {filter} end
+
+	local filterList
+	if type(filter)=="table" then
+		filterList = ffi.new("char*[?]", #filter)
+
+		for k, v in ipairs(filter) do
+			filter[k] = ffi.new("char[?]", #v, v) -- anchor filters in original array
+			filterList[k-1] = filter[k]
+		end
+	end
+
+	return ffi.string(tfd.tinyfd_saveFileDialog(
+		title,
+		path,
+		type(filter)=="table" and #filter or 0,
+		filterList,
+		description))
+end
+
+--fileDialog.fileOpen("Title", "", {"*.jpg", "*.tif"}, "Image Files")
+--fileDialog.fileSave("Title", "", {"*.jpg", "*.tif"}, "Image Files")
+
+return fileDialog
