d553d95c6441d5ac730da5182245e8f867b98525
Galin Bajlekov <g.bajlekov@gmail.com>
2019-11-02 12:24:05 +0100

    Update domain transform filter with separate guide image

--- ops/spec/domainTransform.lua
+++ ops/spec/domainTransform.lua
@@ -2,15 +2,17 @@ return {
 	name = "DT filter",
 	procName = "domainTransform",
 	input = {
-		[0] = {cs = "LAB"},
-		[1] = {cs = "Y"},
+		[0] = {cs = "XYZ"},
+		[1] = {cs = "LAB"},
 		[2] = {cs = "Y"},
+		[3] = {cs = "Y"},
 	},
 	param = {
-		[1] = {type = "float", name = "Distance", min = 0, max = 100, default = 30},
-		[2] = {type = "float", name = "Similarity", min = 0, max = 1, default = 0.2},
+		[1] = {type = "text", left = "Guide Image"},
+		[2] = {type = "float", name = "Distance", min = 0, max = 100, default = 30},
+		[3] = {type = "float", name = "Similarity", min = 0, max = 1, default = 0.2},
 	},
 	output = {
-		[0] = {cs = "LAB"}
+		[0] = {cs = "XYZ"}
 	},
 }
