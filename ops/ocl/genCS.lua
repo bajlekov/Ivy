ec25358e6d6d8d18b4fd6fa3a0d07ae7a945f3a3
Galin Bajlekov <g.bajlekov@gmail.com>
2019-11-23 16:46:26 +0100

    Implement CS conversions using IvyScript

--- ops/ocl/genCS.lua
+++ ops/ocl/genCS.lua
@@ -15,29 +15,60 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
 
-local process = require "lib.opencl.process"
+local proc = require "lib.opencl.process.ivy".new()
 
-local source = {}
-for k, v in ipairs{"SRGB", "LRGB", "XYZ", "LAB", "LCH", "Y", "L"} do
-  local f = assert(io.open("ops/ocl/cs_kernels_"..v..".cl", "rb"))
-  source[v] = f:read("*a")
-  f:close()
+local source = [[
+kernel SRGB(I, O)
+	const x = get_global_id(0)
+	const y = get_global_id(1)
+	O[x, y] = I[x, y].SRGB
 end
 
+kernel LRGB(I, O)
+	const x = get_global_id(0)
+	const y = get_global_id(1)
+	O[x, y] = I[x, y].LRGB
+end
+
+kernel XYZ(I, O)
+	const x = get_global_id(0)
+	const y = get_global_id(1)
+	O[x, y] = I[x, y].XYZ
+end
+
+kernel LAB(I, O)
+	const x = get_global_id(0)
+	const y = get_global_id(1)
+	O[x, y] = I[x, y].LAB
+end
+
+kernel LCH(I, O)
+	const x = get_global_id(0)
+	const y = get_global_id(1)
+	O[x, y] = I[x, y].LCH
+end
+
+kernel Y(I, O)
+	const x = get_global_id(0)
+	const y = get_global_id(1)
+	O[x, y] = I[x, y].Y
+end
+
+kernel L(I, O)
+	const x = get_global_id(0)
+	const y = get_global_id(1)
+	O[x, y] = I[x, y].L
+end
+]]
+
 local function init(d, c, q, name)
-  local proc = process.new()
-
   proc:init(d, c, q)
-  proc:loadSourceString(source[name])
+	proc:loadSourceString(source)
 
-  local function execute()
-    proc:getAllBuffers("in", "out")
-		proc.buffers["in"].__write = false
-		proc.buffers.out.__read = false
-    proc:executeKernel(name, proc:size2D("out"))
+  return function()
+  	local I, O = proc:getAllBuffers(2)
+  	proc:executeKernel(name, proc:size2D(O), {I, O})
   end
-
-  return execute
 end
 
 return init
