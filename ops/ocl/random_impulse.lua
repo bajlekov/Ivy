9c479ab2cd1a6ba8b75a96ea34def87776348c8c
Galin Bajlekov <g.bajlekov@gmail.com>
2020-10-29 10:05:54 +0100

    Independent dark and bright spots in the impulse noise module

--- ops/ocl/random_impulse.lua
+++ ops/ocl/random_impulse.lua
@@ -19,18 +19,20 @@ local proc = require "lib.opencl.process.ivy".new()
 local ffi = require "ffi"
 
 local source = [[
-kernel random(I, W, O, seed)
+kernel random(I, P, O, W, B, seed)
   const x = get_global_id(0)
   const y = get_global_id(1)
   const z = get_global_id(2)
 
-  var w = (1 - W[x, y, z])*0.5
+  var p = (1 - P[x, y, z])*0.5
+  var w = W[x, y, z] > 0.5
+  var b = B[x, y, z] > 0.5
 
   var r = runif(seed+z, x, y)
-  if r > (0.5 + w) then
+  if r > (0.5 + p) and w then
     O[x, y, z] = 1.0
   else
-    if r < (0.5 - w) then
+    if r < (0.5 - p) and b then
       O[x, y, z] = 0.0
     else
       O[x, y, z] = I[x, y, z]
@@ -40,9 +42,9 @@ end
 ]]
 
 local function execute()
-	local I, W, O = proc:getAllBuffers(3)
+	local I, P, W, B, O = proc:getAllBuffers(5)
   local seed = ffi.new("int[1]", math.random( -2147483648, 2147483647))
-	proc:executeKernel("random", proc:size3D(O), {I, W, O, seed})
+	proc:executeKernel("random", proc:size3D(O), {I, P, O, W, B, seed})
 end
 
 local function init(d, c, q)
--- ops/spec/random_impulse.lua
+++ ops/spec/random_impulse.lua
@@ -7,6 +7,8 @@ return {
 	},
 	param = {
 		[1] = {type = "float", name = "Probability", min = 0, max = 0.1, default = 0.01},
+		[2] = {type = "bool", name = "White", default = true},
+		[3] = {type = "bool", name = "Black", default = true},
 	},
 	output = {
 		[0] = {cs = "LRGB"}
